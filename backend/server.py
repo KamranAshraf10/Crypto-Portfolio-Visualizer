86611279449b9b5c0181c2aabcadf10caba2998b
Kamran <techlifeinnovation10@gmail.com>
2024-04-05 19:30:10 +0500

    new

--- backend/server.py
+++ backend/server.py
@@ -1,10 +1,28 @@
 import requests
 from collections import defaultdict
 from datetime import datetime
-import pysopg2 import pool
+from psycopg2 import pool
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
-from logic import BOUGHT,SOLD
-from logic import format_db_row_to_transaction
+
+# from logic import BOUGHT,SOLD
+# from logic import format_db_row_to_transaction
 
 app = Flask(__name__)
+cors = CORS(app)
+
+
+# Database Configuration
+postgreSQL_pool = pool.SimpleConnectionPool(
+    1, 100, dbname="exampledb", user="docker", password="docker", host="0.0.0.0"
+)
+
+app.config["postgresSQL_pool"] = postgreSQL_pool
+
+
+@app.route("/")
+def health_check():
+    return "I am healthy"
+
+
+app.run(debug=True, port=5000)
